0e0719fbf9f301fc38672efa8f25021c38f1976f
Pritam Raha <rahapritam21@yahoo.com>
2023-04-16 12:24:36 +0530

    doubly linked list deletion operation

---
+++ doublyLinkedListDeletionOperation.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+        
+class doublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def insertData(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            return
+        current = self.head
+        while current.next != None:
+            current = current.next
+        current.next = newNode
+        newNode.prev = current
+
+
+    def deleteNode(self):
+        if self.head is None:
+            print("deletion can not be done, as the doubly linked list is already empty") 
+            #return is not required, as i am using else    
+        else:
+            
+            if self.head.next is None:
+                self.head = None
+                #return is not required, as i am using else
+            else:
+                self.head = self.head.next
+
+
+
+
+
+    
+    def __str__(self):
+        if self.head is None:
+            return "Doubly Linked List Is Empty"
+        else:
+            current = self.head
+            container2 = ' '
+            while current != None:
+                container2 = container2 + str(current.data) + ' <-> '
+                current = current.next
+            container2 = container2[:-5]
+            return container2
+  
+
+if __name__ == '__main__':
+    dList = doublyLinkedList()
+    dList.insertData(10)
+    dList.insertData(8)
+    dList.insertData(6)
+    dList.insertData(4)
+    dList.insertData(2)
+    dList.insertData(1)
+    dList.insertData(0)
+    
+    print(dList)
+    
+    dList.deleteNode()
+    
+    print(dList)
