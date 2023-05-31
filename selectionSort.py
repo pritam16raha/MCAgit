4ee446e2d8c7428c12862832a4bc8119533ebddf
Pritam Raha <rahapritam21@yahoo.com>
2023-05-31 22:01:16 +0530

    2nd sem is running

---
+++ selectionSort.py
@@ -0,0 +1,24 @@
+# Selection sort in Python
+
+
+def selectionSort(array, size):
+   
+    for step in range(size):
+        min_idx = step
+
+        for i in range(step + 1, size):
+         
+            # to sort in descending order, change > to < in this line
+            # select the minimum element in each loop
+            if array[i] < array[min_idx]:
+                min_idx = i
+         
+        # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+
+data = [10,1000,99,999,1,99]
+size = len(data)
+selectionSort(data, size)
+print('Sorted Array in Ascending Order:')
+print(data)
